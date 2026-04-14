a9e84df9a959e70e88958bf9708b735c2e2bba48
daniel baptiste <dbaptiste@my.yearupunited.org>
2026-04-14 13:48:36 -0700

    Added queries of actor and film tables

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,3 +1,8 @@
 /*
 Sakila is a database used to collect information about customers and rentals for use by aspiring data analysts.
 */
+
+USE sakila; 
+SELECT * FROM actor; -- Retrieved 200 records
+
+SELECT * FROM film; -- Retrieved 1000 records
