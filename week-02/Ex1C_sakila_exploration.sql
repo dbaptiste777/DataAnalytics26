5bf6dd55dabd5430053839436b5f17a936101b78
daniel baptiste <dbaptiste@my.yearupunited.org>
2026-04-14 15:48:17 -0700

    updated excercise 1C

--- week-02/Ex1C_sakila_exploration.sql
+++ week-02/Ex1C_sakila_exploration.sql
@@ -4,7 +4,8 @@ b) In the schema panel, the "film" table includes a list of films and the descri
 c) The film_actor table also contains actor_id and film_id.
 d) After right clicking on rental, this table includes the rental information for customers of this store. The information is easy to read, however it does not include exact names and has data which can be condensed. 
 e) The inventory table includes what is currently in stock at this film store.
-f) Based on what I learned so far, I need to use the film, rental, and inventory tables to understand the names of all films that were rented on a specific date. The tables are related to eachother because it shows when the film was rented, the name of the film, and wheN.
+f) Based on what I learned so far, I need to use the film, rental, and inventory tables to understand the names of all films that were rented on a specific date. The tables are related to eachother because it shows when the film was rented, the name of the film, and when. 
+*/
 
 
 SELECT film_id FROM film;
