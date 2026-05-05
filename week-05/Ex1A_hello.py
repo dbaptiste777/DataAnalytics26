3cb0df15c26c7c13ebc707f3f66a874ca9d8f324
Daniel Baptiste <dbaptiste919@gmail.com>
2026-05-05 07:56:47 -0700

    1A Homework

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,3 +1,19 @@
 print ('Hello world!')
 
-git config --global user.name Daniel Baptiste
+print()
+message = 'Hello world!'
+print(message)
+
+# "Helo world" prints twice because I told VS code to print it twice. Once in the message and once in the print.
+
+# Displaying dollars and cents
+print()
+
+d_str = '3 dollars'
+c_str = '50 cents'
+
+# The result came back as 3.5 instead of 3.50. Also there is not  dollar sign in front of 3.
+
+
+print(d_str + ' ' + c_str)
+
