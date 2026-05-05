b448d8bb6f02cef54643f19ed693190e1a13c5d1
Daniel Baptiste <dbaptiste919@gmail.com>
2026-05-05 07:25:59 -0700

    First Pthon file

---
+++ week-03/3A_aggregations.sql
@@ -0,0 +1,41 @@
+/* Write a query to find the price of the cheapest item that Northwind sells. Then write a 
+second query to find the name of the product that has that price. */
+
+
+/* Write a query to find the average price of all items that Northwind sells.
+(Bonus: Once you have written a working query, try asking Claude or ChatGPT for help 
+using the ROUND function to round the average price to the nearest cent.) */
+
+
+/* Write a query to find the price of the most expensive item that Northwind sells. Then 
+write a second query to find the name of the product with that price, plus the name of 
+the supplier for that product.*/
+
+
+/* Write a query to find total monthly payroll (the sum of all the employees’ monthly 
+salaries). */
+
+
+/*  Write a query to identify the highest salary and the lowest salary amounts which any 
+employee makes. (Just the amounts, not the specific employees!) */
+
+
+/* Write a query to find the name and supplier ID of each supplier and the number of 
+items they supply. Hint: Join is your friend here. */
+
+
+
+/* Write a query to find the list of all category names and the average price for items in 
+each category. */
+
+
+/* Write a query to find, for all suppliers that provide at least 5 items to Northwind, what 
+is the name of each supplier and the number of items they supply.
+Year Up United Data Analyst Training Academy Week 3 Lab Workbook
+Page 13 of 16 */
+
+
+/* Write a query to list products currently in inventory by the product id, product name, 
+and inventory value (calculated by multiplying unit price by the number of units on 
+hand). Sort the results in descending order by value. If two or more have the same 
+value, order by product name. If a product is not in stock, leave it off the list */
