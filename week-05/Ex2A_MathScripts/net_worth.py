7eb2fee3bec431597ef02f681595628bb5ff95a3
Daniel Baptiste <dbaptiste919@gmail.com>
2026-05-07 11:13:34 -0700

    Net worth

---
+++ week-05/Ex2A_MathScripts/net_worth.py
@@ -0,0 +1,9 @@
+#1.Calculate Net Worth
+assets = 9000
+debts = 3949
+networth = 5051
+
+print ("Your total assets are " + str(assets)) 
+print ("Your total assets are " + str(debts))   
+print ("Your net worth is " + str(networth))
+
