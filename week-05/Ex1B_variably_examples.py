2c2614d3888d6712edcbe97334becd6421ea95cf
Daniel Baptiste <dbaptiste919@gmail.com>
2026-05-07 08:57:38 -0700

    1B Homework

---
+++ week-05/Ex1B_variably_examples.py
@@ -0,0 +1,27 @@
+
+customer_ID = 718917646
+customers_name = 'brenda_grier' #I wrote this first mame + last name without the quotes and underscore and it gave me an error message.
+customers_gender = "female"
+customer_dob = 6/4/1959
+drivers_license_number = 777777777
+auto_policy_number = 9999999999
+
+student_name = 'daniel_baptiste' 
+place_of_birth = 'Brooklyn, NY'
+
+# The full list of reserved words that cannot be used for variavle names include 
+#False	def	if	raise
+#None	del	import	return
+#True	elif	in	try
+#and	else	is	while
+#as	except	lambda	with
+#assert	finally	nonlocal	yield
+#break	for	not
+#class	form	or
+#continue	global	pass
+
+#False - This is a boolean value, and result of a comparison operation.
+#Raise - This raises an exception.
+#Lambda - This creates an anonymous function.
+#In - Checks if a value is present in a list.
+#Def - This keyword defines a function.
