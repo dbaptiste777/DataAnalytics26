734296b7e8d3c375f9e49ba2d09e09b51e7496b7
Daniel Baptiste <dbaptiste919@gmail.com>
2026-05-07 11:13:11 -0700

    Rectangle

---
+++ week-05/Ex2A_MathScripts/area_of_rectangle.py
@@ -0,0 +1,10 @@
+side_a = 9
+side_b = 19
+area = 171
+
+print(side_a * side_b)
+print()
+print("Side A is " + str(side_a))
+print("Side B is " + str(side_b))
+print("The area of the rectangle is " + str(area))
+
