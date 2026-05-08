c3091cc807b0b0ce8502478e2ba519ce64075de9
Daniel Baptiste <dbaptiste919@gmail.com>
2026-05-08 08:52:33 -0700

    update

---
+++ week-05/Ex2B_MoreMathScripts.py/tiles.py
@@ -0,0 +1,19 @@
+
+
+legnth = 18 #room dimentions
+width = 12
+
+print( legnth * width) # 216 total feet
+
+feet = 216 #total feet
+tiles_per = 12 #tiles per box
+
+print(legnth * width / tiles_per) # 18 total boxes
+
+total_boxes = 18
+
+print(round(total_boxes * .1)) # 10% of more tiles will be about 2 extra boxes of tiles.
+
+
+
+
