0e269d25b6f8e57d5cd13a12e584c70daa294ca1
Daniel Baptiste <dbaptiste919@gmail.com>
2026-05-07 17:27:19 -0700

    72

---
+++ week-05/Ex2A_MathScripts/rule_of_72.py
@@ -0,0 +1,10 @@
+current_sav = 7200
+int_rate = 8
+
+formula = 72 / int_rate
+
+print(72/int_rate)
+
+
+print(f"Your current savings is {current_sav:.2f}. At a {int_rate}% interest rate, your savings account will be worth {current_sav*2:.2f} in {formula} years.")
+
