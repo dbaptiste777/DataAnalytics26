b153f67082865c85974790a9dfb16219a35c78f5
Daniel Baptiste <dbaptiste919@gmail.com>
2026-05-08 08:22:23 -0700

    Updated distance homework

--- week-05/Ex2B_MoreMathScripts.py/distance.py
+++ week-05/Ex2B_MoreMathScripts.py/distance.py
@@ -8,9 +8,8 @@ y_2 = 11
 
 # distance formula = Sq ( x2-x1 )^2 + ( y2-y1 )^2
 
-print(f"{(x_2 - x_1) *  (x_2 - x_1) + (y_2 - y_1) * (y_2 - y_1):.2f}")
+# print(f"{(x_2 - x_1) ** 2  + (y_2 - y_1) ** 2:.2f}")
 
-print(math.sqrt(13.81))
+print(f"{math.hypot(2.5, 2.75):.2f}")
 
-
-print(f"{(x_2 - x_1) ** 2  + (y_2 - y_1) ** 2:.2f}")
+      
