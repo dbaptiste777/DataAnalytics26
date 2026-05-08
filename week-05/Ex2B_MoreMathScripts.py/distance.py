54214d4c89e0d92a348a97cc217169d408a981ca
Daniel Baptiste <dbaptiste919@gmail.com>
2026-05-08 08:07:31 -0700

    distance homework

---
+++ week-05/Ex2B_MoreMathScripts.py/distance.py
@@ -0,0 +1,16 @@
+import math
+import cmath
+
+x_1 = 7.5
+x_2 = 10
+y_1 = 8.25
+y_2 = 11
+
+# distance formula = Sq ( x2-x1 )^2 + ( y2-y1 )^2
+
+print(f"{(x_2 - x_1) *  (x_2 - x_1) + (y_2 - y_1) * (y_2 - y_1):.2f}")
+
+print(math.sqrt(13.81))
+
+
+print(f"{(x_2 - x_1) ** 2  + (y_2 - y_1) ** 2:.2f}")
