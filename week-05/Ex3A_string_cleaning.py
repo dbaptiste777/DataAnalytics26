f61887b95bd286425088ba5978819123b556cacf
Daniel Baptiste <dbaptiste919@gmail.com>
2026-05-11 11:54:19 -0700

    Ex 3A

---
+++ week-05/Ex3A_string_cleaning.py
@@ -0,0 +1,21 @@
+name_1 = "PRIYA SHARMA"
+name_2 = "bob NGUYEN"
+name_3 = "LaTonya Williams"
+salary_1 = "$82,500"
+salary_2 = "$74,000"
+
+print(name_1.lower())
+print(name_2.lower())
+print(name_3.lower())
+print()
+print(name_1.title())
+print(name_2.title())
+print(name_3.title())
+print()
+
+
+salary_1 = salary_1.replace(",","")
+print(int(salary_1.replace("$", "")))
+
+
+
