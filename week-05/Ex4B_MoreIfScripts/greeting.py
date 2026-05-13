c4386317ac7e70e6f73e33b5fa2f090aa0f2ede9
Daniel Baptiste <dbaptiste919@gmail.com>
2026-05-13 05:47:39 -0700

    greeting

---
+++ week-05/Ex4B_MoreIfScripts/greeting.py
@@ -0,0 +1,17 @@
+hour = 6
+if hour < 10:
+    print("Good morning!")
+elif hour < 17:
+    print("Good day!")
+else:
+    print:("Good evening!") #Prints Good Morning!
+
+
+
+hour = 2
+if hour >= 23 or hour < 4:
+    print("What are you doing up so late??")
+elif hour < 17:
+    print("Good day!")
+else:
+    print:("Good evening!") #Prints What are you doing up so late??
