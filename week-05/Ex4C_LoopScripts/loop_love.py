cca6398a19e588c963d8972944c7938b0cda273d
Daniel Baptiste <dbaptiste919@gmail.com>
2026-05-14 11:18:08 -0700

    Loop love

---
+++ week-05/Ex4C_LoopScripts/loop_love.py
@@ -0,0 +1,9 @@
+great = ["perfect", "awesome", "incredible", "proficient"] #List fo words
+
+
+# Loop through the list
+for word in great:
+    print("Loops are " + word + "!")
+
+# Final message
+print("I <3 loops")
